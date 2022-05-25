7f045968d97bdaf699286c6199c4a73c781a23f0
Kavetskyi <ykavetsky44@gmail.com>
2022-05-25 14:29:28 +0300

    change4 task_3.py

--- task_3.py
+++ task_3.py
@@ -1,30 +1,16 @@
 # Lecture #2 OOP
 # Task 3
+import json
 
 class DictMixin():
     def to_dict(self):
-         return self._convert_dict(self.__dict__)
+        return self.__dict__
 
-    def _convert_dict(self, attributes):
-        result = {}
-        for key, value in attributes.items():
-            result[key] = self._convert(key, value)
-        return result
+class JSONMixin():
+    def to_json(self):
+        return json.dumps(self.to_dict())
 
-
-    def _convert(self, key, value):
-        if isinstance(value, DictMixin):
-            return value.to_dict()
-        elif isinstance(value, dict):
-            return self._convert_dict(value)
-        elif isinstance(value, list):
-            return [self._convert(key, v) for v in value]
-        elif hasattr(value, '__dict__'):
-            return self._convert_dict(value.__dict__)
-        else:
-            return value
-
-class Profile(DictMixin):
+class Profile(DictMixin, JSONMixin):
     def __init__(self, name, last_name, phone_number, address, email, birthday, age, sex):
         self.name = name
         self.last_name = last_name
@@ -38,28 +24,4 @@ class Profile(DictMixin):
 
 person1 = Profile('Sara', 'Smith', '+39393939', 'Lviv', 'email@email', 'april 22', 30, 'woman')
 print(person1.to_dict())
-
-
-# Task 3 ( )
-
-class DictMixin():
-    def to_dict(self):
-        return self.__dict__
-
-
-class Profile(DictMixin):
-    def __init__(self, name, last_name, phone_number, address, email, birthday, age, sex):
-        self.name = name
-        self.last_name = last_name
-        self.phone_namber = phone_number
-        self.address = address
-        self.email = email
-        self.birthday = birthday
-        self.age = age
-        self.sex = sex
-
-
-person1 = Profile('Sara', 'Smith', '+39393939', 'Lviv', 'email@email', 'april 22', 30, 'woman')
-personinfo1 = person1.to_dict()
-print(f"Hello, my name is {personinfo1['name']} {personinfo1['last_name']}, tel: {personinfo1['phone_namber']}, e-mail {personinfo1['email']}")
-print(f"I live in {personinfo1['address']}, my birthday {personinfo1['birthday']}, I am {personinfo1['age']} years old and I am a {personinfo1['sex']}")
+print(person1.to_json())
